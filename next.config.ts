aeb29da740424010daeb023b7f13e688fa7c35a5
Ayush Nanda <ayushnanda14@gmail.com>
2025-06-22 21:56:14 +0530

    Fixing routing issues

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,17 @@
 const nextConfig = {
   output: "export",   // ⬅ static HTML + JS
   reactStrictMode: true,
+  trailingSlash: false,
   images: {
     unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
     ],
   },
