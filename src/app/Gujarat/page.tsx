a75c22d70b174c84e28e88ff9c710a62cadfb3bb
Babulnath Meher <99674387+babulnath123@users.noreply.github.com>
2026-01-19 20:29:23 +0530

    Update page.tsx

--- src/app/Gujarat/page.tsx
+++ src/app/Gujarat/page.tsx
@@ -1,10 +1,7 @@
-import { Suspense } from 'react';
 import GujaratClient from './GujaratClient';
 
 export default function AboutPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
       <GujaratClient />
-    </Suspense>
   );
 } 
