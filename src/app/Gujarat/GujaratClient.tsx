92713af8cd09fb657e6da6489c7f341432c76b12
Babulnath Meher <99674387+babulnath123@users.noreply.github.com>
2026-01-20 17:31:51 +0530

    Update GujaratClient.tsx

--- src/app/Gujarat/GujaratClient.tsx
+++ src/app/Gujarat/GujaratClient.tsx
@@ -18,6 +18,7 @@ export default function GujaratClient() {
     <>
       <Navbar />
       <h1>Gujarat Landing Page</h1>
+      <p>We are a private registered tour operator based in Ahmedabad - not an official government tourism department. We are known for offering the best tour packages and services that are reliable and budget-friendly as well. For rendering these services with perfection, we have a team of highly skilled tour operators that are backed by years of experience and work in accordance with the client’s requirements. We make sure that the client is satisfied with our service and is left with no complaints. Our main are of operation is Gurajat. We have a very positive reputation in the market and all the credit for that goes to the quality of our services.</p>
       <Footer />
     </>
   );
