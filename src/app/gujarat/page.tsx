097dc26710863bf12ca0841f0952a18faecfd6a5
Babulnath Meher <99674387+babulnath123@users.noreply.github.com>
2026-01-20 16:37:47 +0530

    Create page.tsx for Gujarat Landing Page

---
+++ src/app/gujarat/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Navbar from '@/components/Navbar';
+// import GujaratClient from './GujaratClient';
+import Footer from '@/components/Footer';
+
+export default function GujaratPage() {
+  return (
+    <>
+      <Navbar />
+      {/* <GujaratClient /> */}
+      <Footer />
+    </>
+  );
+}
