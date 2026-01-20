9157bd655f15948c78da4863cd1f55d119bfea7c
Babulnath Meher <99674387+babulnath123@users.noreply.github.com>
2026-01-20 12:25:22 +0530

    Update GujaratClient.tsx

--- src/app/gujarat/GujaratClient.tsx
+++ src/app/gujarat/GujaratClient.tsx
@@ -1,19 +1,9 @@
 'use client';
 
-import { useEffect } from 'react';
 // import LandingPageNavbar from '@/components/LandingPageNavbar';
 import Footer from '@/components/Footer';
 
 export default function GujaratClient() {
-
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.gtag) {
-      window.gtag('event', 'conversion', {
-        send_to: 'AW-17514037101/EksMCJ2ykKUbEO3-q59B',
-      });
-    }
-  }, []);
-
   return (
     <>
       {/* <LandingPageNavbar /> */}
