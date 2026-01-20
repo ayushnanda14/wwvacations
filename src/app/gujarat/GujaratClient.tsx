015994d70d67a2d2f85f0e9c861856c75972bbb4
Babulnath Meher <99674387+babulnath123@users.noreply.github.com>
2026-01-20 16:58:26 +0530

    Update GujaratClient.tsx

--- src/app/gujarat/GujaratClient.tsx
+++ src/app/gujarat/GujaratClient.tsx
@@ -1,15 +1,10 @@
 'use client';
 
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-
 export default function GujaratClient() {
 
   return (
     <>
-      <Navbar />
       <h1>Landing Page For Gujarat</h1>
-      <Footer />
     </>
   );
 } 
