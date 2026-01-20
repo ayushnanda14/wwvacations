d577fde272e6dbed46b4a0b67ceeccd98a62547d
Babulnath Meher <99674387+babulnath123@users.noreply.github.com>
2026-01-20 18:07:31 +0530

    Update GujaratClient.tsx

--- src/app/gujarat-package/GujaratClient.tsx
+++ src/app/gujarat-package/GujaratClient.tsx
@@ -9,8 +9,7 @@ export default function GujaratClient() {
     <>
       <Navbar />
       <h1>Gujarat Landing Page</h1>
-      <p>Gujarat Tour Packages - India No. 1 Destination
-From the Rann of Kutch to the Somnath Temple, let Gujarat's splendor amaze you.</p>
+      <p>Gujarat Tour Packages - India No. 1 Destination From the Rann of Kutch to the Somnath Temple, let Gujarat's splendor amaze you.</p>
       <Footer />
     </>
   );
